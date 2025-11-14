098f8c5f5729a2cc4eedef1196541c90597f6b90
Kaleb Hirshfield <kaleb.hirshfield@protonmail.com>
2025-11-14 16:56:25 +0000

    refactor: remove unused imports in FindWordPanel

--- src/main/java/com/kalebhirshfield/woahcab/components/FindWordPanel.java
+++ src/main/java/com/kalebhirshfield/woahcab/components/FindWordPanel.java
@@ -1,12 +1,6 @@
 package com.kalebhirshfield.woahcab.components;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.kalebhirshfield.woahcab.utils.SupabaseAuth;
-import com.kalebhirshfield.woahcab.utils.SupabaseClient;
-
 import javax.swing.*;
-import java.io.IOException;
 
 public class FindWordPanel extends JPanel {
     public FindWordPanel() {
