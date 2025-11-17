8aac6dee7e55ed57c4d782aed1fd99274b0b333f
Kaleb Hirshfield <kaleb.hirshfield@protonmail.com>
2025-11-17 22:22:15 +0000

    refactor: remove unused import for JsonObject in BrowsePanel

--- src/main/java/com/kalebhirshfield/woahcab/components/BrowsePanel.java
+++ src/main/java/com/kalebhirshfield/woahcab/components/BrowsePanel.java
@@ -2,7 +2,6 @@ package com.kalebhirshfield.woahcab.components;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.kalebhirshfield.woahcab.utils.SupabaseAuth;
 import com.kalebhirshfield.woahcab.utils.SupabaseClient;
 
