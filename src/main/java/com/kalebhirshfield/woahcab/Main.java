3d98ba182392ec443c4288f919b1f72fcf0c6383
Kaleb Hirshfield <kaleb.hirshfield@protonmail.com>
2025-12-11 23:34:28 +0000

    chore: update JDK version and Maven compiler properties to 25

--- src/main/java/com/kalebhirshfield/woahcab/Main.java
+++ src/main/java/com/kalebhirshfield/woahcab/Main.java
@@ -4,7 +4,7 @@ import com.kalebhirshfield.woahcab.utils.WindowManager;
 import javax.swing.*;
 
 public class Main {
-    public static void main(String[] args) {
+    static void main() {
         SwingUtilities.invokeLater(WindowManager::new);
     }
 }
